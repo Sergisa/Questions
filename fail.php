cc5fc167961e4aeb09d15be79408b903ae3f623c
Sergisa <phebra@yandex.ru>
2021-11-11 13:22:02 +0300

    страница fail

---
+++ fail.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'functions.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style/style.min.css" rel="stylesheet">
+    <title>Вопросник</title>
+</head>
+<body>
+
+<div class="row">
+    <div class="col-md-7 mx-auto">
+        <div class="card question-card">
+            <div class="card-body">
+                <h5 class="card-title text-danger">Увы ваш тест завершён.</h5>
+                <p class="card-text">
+                    Ой, мы забыли сказать что у вас не было права на ошибку.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
